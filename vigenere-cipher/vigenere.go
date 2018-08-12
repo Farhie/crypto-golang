c8f2474879ef82cf123297902b3d725dd3727689
Adam Fahie <adam.fahie@gmail.com>
2018-08-11 22:07:27 -0700

    [Adam] extract unicode -> ascii number logic

--- vigenere-cipher/vigenere.go
+++ vigenere-cipher/vigenere.go
@@ -12,9 +12,16 @@ func VigenereEncrypt(message, key string) string {
 	messageRune := bytes.Runes([]byte(message))
 	keyRune := bytes.Runes([]byte(key))
 	var cipherText []rune
+
 	for i := range messageRune {
-		encryptedValue := ((messageRune[i] - utf8LetterToNumberOffset + keyRune[i] - utf8LetterToNumberOffset) % 26) + utf8LetterToNumberOffset
+		encryptedValue := computerEncryptedValue(messageRune[i], keyRune[i])
 		cipherText = append(cipherText, encryptedValue)
 	}
+
 	return string(cipherText)
+}
+
+func computerEncryptedValue(messageByte, keyByte rune) rune {
+	var utf8LetterToNumberOffset int32 = 97
+	return ((messageByte - utf8LetterToNumberOffset + keyByte - utf8LetterToNumberOffset) % 26) + utf8LetterToNumberOffset
 }
