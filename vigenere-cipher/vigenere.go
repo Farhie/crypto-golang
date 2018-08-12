2119678d51f15b4d9f8088ff305a624747bb9003
Adam Fahie <adam.fahie@gmail.com>
2018-08-11 22:15:37 -0700

    [Adam] Clean up

--- vigenere-cipher/vigenere.go
+++ vigenere-cipher/vigenere.go
@@ -7,10 +7,8 @@ import (
 	"unicode/utf8"
 )
 
-const utf8LetterToNumberOffset int32 = 97
-
 func VigenereEncrypt(message, key string) string {
-	fmt.Println("Encrypting")
+	fmt.Println("Encrypting using Vigenere cipher")
 	messageRune := bytes.Runes([]byte(message))
 	keyRune := bytes.Runes([]byte(strings.Repeat(key, utf8.RuneCountInString(message))))
 	var cipherText []rune
@@ -24,6 +22,6 @@ func VigenereEncrypt(message, key string) string {
 }
 
 func computerEncryptedValue(messageByte, keyByte rune) rune {
-	var utf8LetterToNumberOffset int32 = 97
-	return ((messageByte - utf8LetterToNumberOffset + keyByte - utf8LetterToNumberOffset) % 26) + utf8LetterToNumberOffset
+	var utf8ToAsciiOffset int32 = 97
+	return ((messageByte - utf8ToAsciiOffset + keyByte - utf8ToAsciiOffset) % 26) + utf8ToAsciiOffset
 }
